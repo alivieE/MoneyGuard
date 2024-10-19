67830eb1adf186863095756ac132401eb3e5eb13
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2024-10-19 17:23:06 +0300

    add currencies

--- moneyguard/src/components/SideBar/SideBar.jsx
+++ moneyguard/src/components/SideBar/SideBar.jsx
@@ -1,31 +1,48 @@
-import React, { useState, useEffect } from "react";
-import s from "./SideBar.module.css";
-import { NavLink } from "react-router-dom";
-import Image from "../../assets/index";
+import React, { useState, useEffect } from 'react'
+import s from './SideBar.module.css'
+import { NavLink } from 'react-router-dom'
+import Image from '../../assets/index'
 
 const SideBar = () => {
-  const [page, setPage] = useState("home");  
-
+  const [page, setPage] = useState('home')
+  const [currencies, setCurrencies] = useState(()=>{
+    return JSON.parse(localStorage.getItem('currencies')) || {
+      dollar:{
+        rateBuy:'-',
+        rateSell:'-'
+      },
+      euro:{
+        rateBuy:'-',
+        rateSell:'-'
+      }
+    }
+  });
   useEffect(() => {
-  
-      fetch('https://api.monobank.ua/bank/currency').then((data)=>{
+    fetch('https://api.monobank.ua/bank/currency')
+      .then((data) => {
         return data.json()
-      }).then((res)=>{
-console.log(res);
-if(res.errCode === 'TMR'){
-  console.log('too many requests');
+      })
+      .then((res) => {
+        console.log(res)
+        if (res.errCode === 'TMR') {
+          console.log('too many requests')
 
-  
-  return
+          return
         }
-        console.log(res.find((currency)=>{
+
+        const dollar = res.find((currency) => {
           return currency.currencyCodeA === 840
-        }));
-      }).catch((error)=> {
-      console.log(error);
-     
-    })
-  }, []);
+        })
+        const euro = res.find((currency) => {
+          return currency.currencyCodeA === 978
+        })
+        localStorage.setItem('currencies', JSON.stringify({dollar,euro}))
+        setCurrencies({dollar,euro})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
   return (
     <aside className={s.sideBar}>
@@ -33,14 +50,17 @@ if(res.errCode === 'TMR'){
         <div className={s.homeLink}>
           <NavLink
             onClick={() => {
-              setPage("home");
+              setPage('home')
             }}
             className={s.link}
-            style={{ fontWeight: page === "home" ? "bold" : "normal" }}
+            style={{ fontWeight: page === 'home' ? 'bold' : 'normal' }}
             to="home"
           >
             <div className="activeWrap">
-              <img className={s.homeImage} src={page==='home'? Image.homeWhite:Image.home }></img>
+              <img
+                className={s.homeImage}
+                src={page === 'home' ? Image.homeWhite : Image.home}
+              ></img>
             </div>
             Home
           </NavLink>
@@ -48,45 +68,49 @@ if(res.errCode === 'TMR'){
         <div></div>
         <NavLink
           onClick={() => {
-            setPage("statistic");
+            setPage('statistic')
           }}
           className={s.link}
-          style={{ fontWeight: page === "statistic" ? "bold" : "normal" }}
+          style={{ fontWeight: page === 'statistic' ? 'bold' : 'normal' }}
           to="statistic"
         >
           <div className="activeWrap">
-            <img className={s.statisticImage} src={page==='statistic'?Image.statisticWhite:Image.statistic}></img>
+            <img
+              className={s.statisticImage}
+              src={
+                page === 'statistic' ? Image.statisticWhite : Image.statistic
+              }
+            ></img>
           </div>
           Statistic
         </NavLink>
-
       </div>
       <div className={s.balance}>
         <p className={s.text}>YOUR BALANCE</p>
         <div className={s.balanceCount}>
           <p className={s.currency}>₴</p>
           <p className={s.balanceValue}> 24 000.00</p>
-          </div>
-      </div>    
+        </div>
+      </div>
       <table className={s.table}>
-          <tr className={s.trTH}>
-            <th className={s.th}>Currency</th>
-            <th className={s.th}>Purchase</th>
-            <th className={s.th}>Sale</th>
-          </tr>
-          <tr className={s.tr}>
-            <td className={s.tdUSD}>USD</td>
-            <td className={s.tdUSD}>27.55</td>
-            <td className={s.tdUSD}>27.65</td>
-          </tr>
-          <tr className={s.tr}>
-            <td className={s.tdEUR}>EUR</td>
-            <td className={s.tdEUR}>30.00</td>
-            <td className={s.tdEUR}>30.10</td>
-          </tr>
-      </table>  
+        <tr className={s.trTH}>
+          <th className={s.th}>Currency</th>
+          <th className={s.th}>Purchase</th>
+          <th className={s.th}>Sale</th>
+        </tr>
+        <tr className={s.tr}>
+          <td className={s.tdUSD}>USD</td>
+          <td className={s.tdUSD}>{currencies.dollar.rateSell}</td>
+          <td className={s.tdUSD}>{currencies.dollar.rateBuy}</td>
+        </tr>
+        <tr className={s.tr}>
+          <td className={s.tdEUR}>EUR</td>
+          <td className={s.tdEUR}>{currencies.euro.rateSell}</td>
+          <td className={s.tdEUR}>{currencies.euro.rateBuy}</td>
+        </tr>
+      </table>
     </aside>
-  );
-};
+  )
+}
 
-export default SideBar;
+export default SideBar

6024e85e1ac446bc985a8aaa0f24ca8e6865a219
alivieE <153372860+alivieE@users.noreply.github.com>
2024-11-02 19:34:26 +0200

    its work

--- moneyguard/src/components/Home/Home.jsx
+++ moneyguard/src/components/Home/Home.jsx
@@ -1,15 +1,13 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import AddModal from '../AddModal/AddModal';
-import s from './Home.module.css'
-import Image from '../../assets/index'
+import s from './Home.module.css';
+import Image from '../../assets/index';
+
 const Home = () => {
-const [isOpen, setIsOpen] = useState(false);
-const [transactions, setTransactions] = useState(() =>{
-  return JSON.parse(localStorage.getItem('transactions')) || []
-
-  
-});
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [transactions, setTransactions] = useState(() => {
+    return JSON.parse(localStorage.getItem('transactions')) || [];
+  });
 
   return (
     <div className={s.Home}>
@@ -20,30 +18,28 @@ const [transactions, setTransactions] = useState(() =>{
             <th className={s.th}>Type</th>
             <th className={s.th}>Category</th>
             <th className={s.th}>Comment</th>
-            <th className={s.th}>Sum</th>    
+            <th className={s.th}>Sum</th>
             <th className={s.th}></th>
             <th className={`${s.th} ${s.radiusRight}`}></th>
           </tr>
         </thead>
-        <tbody>  
-          {transactions.map((transaction) => {
-            return(<tr className={s.trAny}>
+        <tbody>
+          {[...transactions].reverse().map((transaction) => (
+            <tr className={s.trAny}>
               <td className={s.td}>{transaction.date}</td>
               <td className={s.td}>{transaction.type === 'income' ? '+' : '-'}</td>
               <td className={s.td}>{transaction.category || "Other"}</td>
               <td className={s.td}>{transaction.comment}</td>
               <td className={s.td}>{transaction.amount}</td>
-              <td><img src={Image.pencil}/></td>
+              <td><img src={Image.pencil} alt="Edit" /></td>
               <td><button className={s.btnDelete}>Delete</button></td>
-            </tr>)
-          })}
-        </tbody>       
+            </tr>
+          ))}
+        </tbody>
       </table>
-      
-      <AddModal transactions={transactions} setTransactions={setTransactions}/>
-     
+      <AddModal transactions={transactions} setTransactions={setTransactions} />
     </div>
-  )
-}
+  );
+};
 
 export default Home;

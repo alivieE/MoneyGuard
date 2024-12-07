31f3409153fc3309477ddfea0f85b0fe2b99c51b
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2024-12-07 14:27:18 +0200

    fix colors and minor refactoring transaction table

--- moneyguard/src/components/Statistic/Statistic.jsx
+++ moneyguard/src/components/Statistic/Statistic.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
-import AddModal from "../AddModal/AddModal";
-import { BarLabel } from "@mui/x-charts";
 import s from "./Statistic.module.css";
 
 const Statistic = () => {
@@ -17,9 +15,17 @@ const Statistic = () => {
     Other: 0,
   };
 
-
-
-const colors = ['#FED057', '#FFD8D0', '#FD9498', '#C5BAFF', '#6E78E8', '#4A56E2', '#81E1FF','#24CCA7', '#00AD84']
+  const colors = [
+    "#FED057",
+    "#FFD8D0",
+    "#FD9498",
+    "#C5BAFF",
+    "#6E78E8",
+    "#4A56E2",
+    "#81E1FF",
+    "#24CCA7",
+    "#00AD84",
+  ];
 
   const [transactions, setTransactions] = useState(() => {
     return JSON.parse(localStorage.getItem("transactions")) || [];
@@ -35,8 +41,7 @@ const colors = ['#FED057', '#FFD8D0', '#FD9498', '#C5BAFF', '#6E78E8', '#4A56E2'
       return transaction;
     });
 
-
-    const IncomeTransactions = transactions
+  const IncomeTransactions = transactions
     .filter((transaction) => {
       return transaction.type === "income";
     })
@@ -44,21 +49,29 @@ const colors = ['#FED057', '#FFD8D0', '#FD9498', '#C5BAFF', '#6E78E8', '#4A56E2'
       return transaction.amount;
     });
 
-    
-  const valuesCategories = Object.keys(sumCategories).map((type) => {
-    return { label: type, value: sumCategories[type] };
-  });
-
-  const totalExpenses = ExpenseTransactions.reduce((acc, transaction) => acc + transaction.amount, 0);
-  const totalIncome = IncomeTransactions.reduce((acc, amount) => acc + amount, 0);
-  
+  const valuesCategories = Object.keys(sumCategories)
+    .map((type) => {
+      return { label: type, value: sumCategories[type] };
+    })
+    .sort((category1, category2) => {
+      return category2.value - category1.value;
+    });
+  console.log(valuesCategories);
+  const totalExpenses = ExpenseTransactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+  const totalIncome = IncomeTransactions.reduce(
+    (acc, amount) => acc + amount,
+    0
+  );
 
   return (
     <div className={s.statistic}>
       <h1 className={s.Name}>Statistics</h1>
       <div className={s.chartWrap}>
         <PieChart
-        colors={colors}
+          colors={colors}
           height={600}
           width={608}
           options={{
@@ -86,30 +99,38 @@ const colors = ['#FED057', '#FFD8D0', '#FD9498', '#C5BAFF', '#6E78E8', '#4A56E2'
 
       <div className={s.list}>
         <div>
-        <ul>
-          {[...transactions].filter((transaction) => {
-            return transaction.type != "income"
-          }).map((transaction) => (
-            
-              <li key={transaction.id} className={s.listLI}>
-                <div className={s.liWrap}>
-                  <div className={s.boxColor}>{}</div>
-                  <p>{transaction.category}</p>
-                </div>
-                <p>{transaction.amount}</p>
-                
-              </li>            
-          ))} 
+          <ul>
+            {valuesCategories.map((transaction, index) => {
+              if (transaction.value <= 0) {
+                return null;
+              }
+              return (
+                <li key={transaction.id} className={s.listLI}>
+                  <div className={s.liWrap}>
+                    <div
+                      style={{ backgroundColor: colors[index] }}
+                      className={s.boxColor}
+                    ></div>
+                    <p>{transaction.label}</p>
+                  </div>
+                  <p>{transaction.value}</p>
+                </li>
+              );
+            })}
+          </ul>
+          <ul className={s.totalWrap}>
+            <li className={s.expense}>
+              <p>Expense:</p>
+              <p>{totalExpenses}</p>
+            </li>
+            <li className={s.income}>
+              <p>Income:</p>
+              <p>{totalIncome}</p>
+            </li>
           </ul>
         </div>
       </div>
-      <div className={s.total}>
-        <div className={s.totalWrap}>
-          <p className={s.expense}>Expense: <p className={s.totalAmoutExpense}>{totalExpenses}</p></p>
-          <p className={s.income}>Income: <p className={s.totalAmoutIncome}>{totalIncome}</p></p>
-        </div>
-      </div>
-     
+      <div className={s.total}></div>
     </div>
   );
 };
